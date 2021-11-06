2e2ac0cf65802984d6374c17c57f1ec18e0b2f83
jihye <jiji.pak41@gmail.com>
2021-11-06 20:03:01 +0000

    li tag for list

--- src/TasksList.jsx
+++ src/TasksList.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 export default function TasksList(props) {
   // const storage = window.localStorage;
   return (
-    <>
+    <li className="task">
       <span className="task-name">Coding</span>
       <span className="task-count">0</span>
       <button className="task-plus task-btn">➕</button>
       <button className="task-minus task-btn">➖</button>
       <button className="task-bin task-btn">🗑</button>
-    </>
+    </li>
   );
 }
