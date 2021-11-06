d6c78c08b850eae76a9175b936c8b79ccce030f7
jihye <jiji.pak41@gmail.com>
2021-11-06 19:59:46 +0000

    create task list and style it

--- src/TasksList.jsx
+++ src/TasksList.jsx
@@ -2,5 +2,13 @@ import React from 'react';
 
 export default function TasksList(props) {
   // const storage = window.localStorage;
-  return 'Task list';
+  return (
+    <>
+      <span className="task-name">Coding</span>
+      <span className="task-count">0</span>
+      <button className="task-plus task-btn">➕</button>
+      <button className="task-minus task-btn">➖</button>
+      <button className="task-bin task-btn">🗑</button>
+    </>
+  );
 }
