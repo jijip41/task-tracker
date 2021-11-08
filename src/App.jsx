675129d70e1a0d128c59a4c17825e3786c7dab12
jihye <jiji.pak41@gmail.com>
2021-11-08 23:47:58 +0000

    move state to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,45 @@ import Form from './components/Form';
 import Reset from './components/Reset';
 
 function App() {
+  const [tasks, setTasks] = React.useState([
+    { id: 1, name: 'Coding', count: 0 },
+    { id: 2, name: 'Reading', count: 0 },
+    { id: 3, name: 'Yoga', count: 0 },
+  ]);
+
+  const handleIncrement = (task) => {
+    const newTasks = [...tasks];
+    const index = newTasks.indexOf(task);
+    newTasks[index].count++;
+    setTasks(newTasks);
+  };
+
+  const handleDecrement = (task) => {
+    const newTasks = [...tasks];
+    const index = newTasks.indexOf(task);
+    const count = newTasks[index].count - 1;
+    newTasks[index].count = count < 0 ? 0 : count;
+    setTasks(newTasks);
+  };
+
+  const handleDelete = (task) => {
+    const newTasks = [...tasks];
+    const index = newTasks.indexOf(task);
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+    // const newTasks = tasks.filter((i) => i.id != task.id);
+    // setTasks(newTasks)
+  };
   return (
     <>
       <Navbar />
       <Form />
-      <Tasks />
+      <Tasks
+        tasks={tasks}
+        handleIncrement={handleIncrement}
+        handleDecrement={handleDecrement}
+        handleDelete={handleDelete}
+      />
       <Reset />
     </>
   );
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -3,47 +3,18 @@ import TasksList from './TasksList';
 
 // Container Component
 
-export default function Tasks() {
+export default function Tasks(props) {
   // State => Array
-  const [tasks, setTasks] = React.useState([
-    { id: 1, name: 'Coding', count: 0 },
-    { id: 2, name: 'Reading', count: 0 },
-    { id: 3, name: 'Yoga', count: 0 },
-  ]);
-
-  const handleIncrement = (task) => {
-    const newTasks = [...tasks];
-    const index = newTasks.indexOf(task);
-    newTasks[index].count++;
-    setTasks(newTasks);
-  };
-
-  const handleDecrement = (task) => {
-    const newTasks = [...tasks];
-    const index = newTasks.indexOf(task);
-    const count = newTasks[index].count - 1;
-    newTasks[index].count = count < 0 ? 0 : count;
-    setTasks(newTasks);
-  };
-
-  const handleDelete = (task) => {
-    const newTasks = [...tasks];
-    const index = newTasks.indexOf(task);
-    newTasks.splice(index, 1);
-    setTasks(newTasks);
-    // const newTasks = tasks.filter((i) => i.id != task.id);
-    // setTasks(newTasks)
-  };
 
   return (
     <ul>
-      {tasks.map((task) => (
+      {props.tasks.map((task) => (
         <TasksList
           key={task.id}
           task={task}
-          onIncrement={handleIncrement}
-          onDecrement={handleDecrement}
-          onDelete={handleDelete}
+          onIncrement={props.handleIncrement}
+          onDecrement={props.handleDecrement}
+          onDelete={props.handleDelete}
         />
       ))}
     </ul>
