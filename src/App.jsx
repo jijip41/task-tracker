018b3c7d9a645858f24a92e6bb9089ae8c9b0665
jihye <jiji.pak41@gmail.com>
2021-11-13 22:41:59 +0000

    remove commented out code

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,12 @@ import Reset from './components/Reset';
 
 function App() {
   const [tasks, setTasks] = React.useState([]);
-  const [allCount, setAllCount] = React.useState(0);
-  // const [tasksCount, setTasksCount] = React.useState(0);
 
   const handleIncrement = (task) => {
     const newTasks = [...tasks];
     const index = newTasks.indexOf(task);
     newTasks[index].count++;
-    // const count = newTasks[index].count;
-    // count === 1 ? setAllCount(allCount + 1) : setAllCount(allCount);
+
     setTasks(newTasks);
   };
 
@@ -25,15 +22,12 @@ function App() {
     const count = newTasks[index].count;
     newTasks[index].count = count < 1 ? count : count - 1;
     setTasks(newTasks);
-    // count === 1 ? setAllCount(count - 1) : setAllCount(allCount);
   };
 
   const handleDelete = (task) => {
     const newTasks = [...tasks];
     const index = newTasks.indexOf(task);
-    // const p = task.count;
 
-    // p === 0 ? setAllCount(allCount) : setAllCount(allCount - 1);
     newTasks.splice(index, 1);
     setTasks(newTasks);
     // const newTasks = tasks.filter((i) => i.id != task.id);
