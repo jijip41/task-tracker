cf808969967bc92320839206fb5421fd4634dc13
jihye <jiji.pak41@gmail.com>
2021-11-13 22:39:18 +0000

    use filter to caculate total tasks

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,8 @@ function App() {
     const newTasks = [...tasks];
     const index = newTasks.indexOf(task);
     newTasks[index].count++;
-    const count = newTasks[index].count;
-    count === 1 ? setAllCount(allCount + 1) : setAllCount(allCount);
+    // const count = newTasks[index].count;
+    // count === 1 ? setAllCount(allCount + 1) : setAllCount(allCount);
     setTasks(newTasks);
   };
 
@@ -25,15 +25,15 @@ function App() {
     const count = newTasks[index].count;
     newTasks[index].count = count < 1 ? count : count - 1;
     setTasks(newTasks);
-    count === 1 ? setAllCount(count - 1) : setAllCount(allCount);
+    // count === 1 ? setAllCount(count - 1) : setAllCount(allCount);
   };
 
   const handleDelete = (task) => {
     const newTasks = [...tasks];
     const index = newTasks.indexOf(task);
-    const p = task.count;
+    // const p = task.count;
 
-    p === 0 ? setAllCount(allCount) : setAllCount(allCount - 1);
+    // p === 0 ? setAllCount(allCount) : setAllCount(allCount - 1);
     newTasks.splice(index, 1);
     setTasks(newTasks);
     // const newTasks = tasks.filter((i) => i.id != task.id);
@@ -50,7 +50,7 @@ function App() {
   };
   return (
     <>
-      <Navbar allCount={allCount} />
+      <Navbar allCount={tasks.filter((i) => i.count > 0).length} />
       <Form addTasks={addTasks} />
       <Tasks
         tasks={tasks}
